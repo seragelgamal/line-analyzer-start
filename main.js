b5cacbdf354d1aa515389ce22eac6d7649844ddb
seragelgamal <55516965+seragelgamal@users.noreply.github.com>
2020-07-03 10:35:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ function analyzeLine() {
 
     // Call Analyze Functions and Display results
     document.getElementById('length').innerHTML = getLength(pt1x, pt1y, pt2x, pt2y);
-    // document.getElementById('slope').innerHTML = getSlope(pt1x, pt1y, pt2x, pt2y);
-    // document.getElementById('description').innerHTML = getDescription(pt1x, pt1y, pt2x, pt2y);
-    // document.getElementById('location-1').innerHTML = getPointLocation(pt1x, pt1y);
-    // document.getElementById('location-2').innerHTML = getPointLocation(pt2x, pt2y);
-    // document.getElementById('equation').innerHTML = getEquation(pt1x, pt1y, pt2x, pt2y);
+    document.getElementById('slope').innerHTML = getSlope(pt1x, pt1y, pt2x, pt2y);
+    document.getElementById('description').innerHTML = getDescription(pt1x, pt1y, pt2x, pt2y);
+    document.getElementById('location-1').innerHTML = getPointLocation(pt1x, pt1y);
+    document.getElementById('location-2').innerHTML = getPointLocation(pt2x, pt2y);
+    document.getElementById('equation').innerHTML = getEquation(pt1x, pt1y, pt2x, pt2y);
 }
 
 // Line Analyzer Functions (Write your solutions here... getLength is done for you)
@@ -29,5 +29,52 @@ function getLength(x1, y1, x2, y2) {
     return (rise ** 2 + run ** 2) ** 0.5
 }
 
+function getSlope(x1, y1, x2, y2) {
+    let rise = y2-y1;
+    let run = x2-x1;
+    return rise/run;
+}
 
+function getDescription(x1, y1, x2, y2) {
+    let slope = getSlope(x1, y1, x2, y2);
+    if ((slope == Infinity) || (slope == -Infinity)) {
+        return 'vertical';
+    } else if (slope > 0) {
+        return 'increasing';
+    } else if (slope < 0) {
+        return 'decreasing';
+    } else {
+        return 'horizontal';
+    }
+}
 
+function getPointLocation(x, y) {
+    if ((x == 0) && (y == 0)) {
+        return 'origin';
+    } else if (y == 0) {
+        return 'x-axis';
+    } else if (x == 0) {
+        return 'y-axis';
+    } else if ((x > 0) && (y > 0)) {
+        return 'quadrant 1';
+    } else if ((x < 0) && (y > 0)) {
+        return 'quadrant 2';
+    } else if ((x < 0) && (y < 0)) {
+        return 'quadrant 3';
+    } else {
+        return 'quadrant 4';
+    }
+}
+
+function getEquation(x1, y1, x2, y2) {
+    let description = getDescription(x1, y1, x2, y2);
+    if (description == 'vertical') {
+        return 'x = ' + x1;
+    } else if (description == 'horizontal') {
+        return 'y = ' + y1;
+    } else {
+        let slope = getSlope(x1, y1, x2, y2);
+        let yInt = y1 - (slope * x1);
+        return 'y = ' + slope + 'x + ' + yInt;
+    }
+}
